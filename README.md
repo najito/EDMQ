40050a514d53c9c452fc95b9724a9b1904c9dbc8
Nobuhide Ajito <najito@Nobuhides-MacBook-Pro.local>
2020-11-12 22:24:47 -0800

    initial frontend setup with header and CRA

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EDMQ
 EDM Quiz
+
+This application was inspired by AMQ to quiz your knowledge on another genre
---
+++ edmq/src/components/Content.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const Content = () => {
+
+  return ( 
+    <Fragment>
+      <p>login</p>
+      <p>about</p>
+    </Fragment>
+   );
+}
+ 
+export default Content;
