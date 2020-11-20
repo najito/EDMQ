2d1dd047b9e4ffa0fe6099c747862e59a810b5e0
unknown <jasonli6395@gmail.com>
2020-11-20 11:37:15 -0800

    Revert "Background image and text boxes"
    This reverts commit 7cbb85bdd8e883a108dabf8d57b7314e4d52fbcc.

--- edmq/src/components/Content.jsx
+++ edmq/src/components/Content.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Login from './Login'
 import About from './About'
+
 const Content = () => {
 
   return ( 
--- edmq/src/components/Main.jsx
+++ edmq/src/components/Main.jsx
@@ -1,9 +1,15 @@
 import React, { Fragment } from 'react';
-import Content from './Content';
+import Content from './Content'
 
 const Main = (props) => {
+
+  const images = {
+    background: require('../assets/backgroundSplash.jpg')
+  };
+
   return ( 
     <Fragment>
+      <img id="background" src={images.background} alt="background"/>
       <div className="App-header">
         <h2>Electronic Dance Music Quiz</h2>
       </div>
