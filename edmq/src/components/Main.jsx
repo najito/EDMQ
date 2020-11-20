6e1af9726fbe317bdc6913a82750ca69f9d026f6
unknown <jasonli6395@gmail.com>
2020-11-20 11:41:36 -0800

    background image and text boxes

--- edmq/src/components/Main.jsx
+++ edmq/src/components/Main.jsx
@@ -2,14 +2,8 @@ import React, { Fragment } from 'react';
 import Content from './Content'
 
 const Main = (props) => {
-
-  const images = {
-    background: require('../assets/backgroundSplash.jpg')
-  };
-
   return ( 
     <Fragment>
-      <img id="background" src={images.background} alt="background"/>
       <div className="App-header">
         <h2>Electronic Dance Music Quiz</h2>
       </div>
